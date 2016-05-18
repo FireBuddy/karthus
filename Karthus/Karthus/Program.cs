7ecba2cafd9329ff2c5ac7a9f24a90f3e1fbda76
AkumaCORE <k-sintu@hotmail.com>
2016-05-18 14:57:20 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -708,7 +708,7 @@ namespace Karthus
                     }
 
                     var predQ = Q2.GetPrediction(qTarget);
-                    if (!cz && predQ.HitChance >= HitChance.High && player.Distance(eTarget.ServerPosition) <= E.Range)
+                    if (!cz && predQ.HitChance >= HitChance.High && player.Distance(qTarget.ServerPosition) <= predQ.CastPosition)
                     {
                         Q.Cast(predQ.CastPosition + 75);
                         Q.Cast(predQ.CastPosition + 50);
