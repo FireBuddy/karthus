3c7a64a39cd6eaf1d69485ca8019077fa848d1b3
AkumaCORE <k-sintu@hotmail.com>
2016-05-18 14:15:57 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -708,10 +708,10 @@ namespace Karthus
                     }
 
                     var predQ = Q2.GetPrediction(qTarget);
-                    var turret = GetNearestEnemyTurret();
+                    //var turret = GetNearestEnemyTurret();
                     if (!cz && predQ.HitChance >= HitChance.High)
                     {
-                        Q.Cast(predQ.CastPosition + (75 - turret.Distance()));
+                        Q.Cast(predQ.CastPosition + 75);
                         Q.Cast(predQ.CastPosition + 50);
                         Q.Cast(predQ.CastPosition + 25);
                     }
