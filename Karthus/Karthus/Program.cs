a97926a9f303ee15cc2a4b3a8bf47b62ba834b0b
AkumaCORE <k-sintu@hotmail.com>
2016-05-18 13:58:09 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -710,7 +710,7 @@ namespace Karthus
                     var predQ = Q2.GetPrediction(qTarget);
                     if (!cz && predQ.HitChance >= HitChance.High)
                     {
-                        Q.Cast(predQ.CastPosition + 75);
+                        Q.Cast(predQ.CastPosition + 75 - enemyTower.Distance(,200));
                         Q.Cast(predQ.CastPosition + 50);
                         Q.Cast(predQ.CastPosition + 25);
                     }
