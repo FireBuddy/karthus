f7a6e706715f4418607d65cd9aa5f1868447c9ba
AkumaCORE <k-sintu@hotmail.com>
2016-05-19 13:25:20 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -188,7 +188,7 @@ namespace Karthus
         private static void Obj_AI_Base_OnBasicAttack(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
             {
-                Q.Cast(); 
+                Q.Cast(Target); 
                 return;
             }
         }
