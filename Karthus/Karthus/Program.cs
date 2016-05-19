9fe863738ce98a5680a7f9f6798be0e4231718ee
AkumaCORE <k-sintu@hotmail.com>
2016-05-19 13:10:00 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -187,6 +187,13 @@ namespace Karthus
         
         private static void Obj_AI_Base_OnBasicAttack(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
+            var predq = Q.GetPrediction(Sender);
+            if (Sender.IsValidTarget(Q.Range) && Q.IsReady() && !Sender.IsAlly && !Sender.IsMe)
+            {
+                {
+                    Q.Cast(predq.CastPosition);
+                }
+            }
             
         }
 
