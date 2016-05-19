96410c624306e10f6e3fd75f2f46b46dc5f794d0
AkumaCORE <k-sintu@hotmail.com>
2016-05-19 12:57:35 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -181,7 +181,6 @@ namespace Karthus
             Game.OnUpdate += OnUpdate;
             Drawing.OnDraw += OnDraw;
             Gapcloser.OnGapcloser += Gapcloser_OnGap;
-            Obj_AI_Base.OnBasicAttack += OnBasicAttack;
         }
         
         
