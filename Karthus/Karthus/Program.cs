1e5752eef8473bc67c5831f3cf7c03cc205abbef
AkumaCORE <k-sintu@hotmail.com>
2016-05-19 20:41:40 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -187,7 +187,7 @@ namespace Karthus
         
         private static void Obj_AI_Base_OnBasicAttack(Obj_AI_Base Sender, GameObjectProcessSpellCastEventArgs args)
         {
-            if (Sender == null)
+            if (Sender == null && !Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
             {
                return;
             }
