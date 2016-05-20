4b423def08e9500452afbb2742a716a7745eacdc
AkumaCORE <k-sintu@hotmail.com>
2016-05-20 10:11:01 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -195,7 +195,7 @@ namespace Karthus
             if (Sender.IsValidTarget(Q.Range) && Q.IsReady() && !Sender.IsAlly && !Sender.IsMe && !Sender.IsMinion && !Sender.IsMonster)
             {
                 {
-                    Q.Cast(yourPos.Extend(enemyPos, Q.Range));
+                    Q.Cast(Sender.ServerPosition + 20);
                 }
             } 
         }
