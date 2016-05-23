2f0474cd3b15136e4ea442e5ad2a8d3529db4864
AkumaCORE <k-sintu@hotmail.com>
2016-05-23 12:37:56 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -193,11 +193,14 @@ namespace Karthus
             }
             if (!Sender.IsDashing() && Sender.Type == GameObjectType.AIHeroClient && Sender.IsValidTarget(Q.Range) && Q.IsReady() && Sender.IsEnemy)
             {
-                if (ObjectManager.Player.Position.Distance(qTarget.ServerPosition) <= 800)
+                if (ObjectManager.Player.Position.Distance(qTarget.ServerPosition) <= 825)
                 {
-                    Q.Cast(Sender.ServerPosition + 75)
+                    Q.Cast(Sender.ServerPosition + 50);
                 }
-               
+                if (ObjectManager.Player.Position.Distance(qTarget.ServerPosition) > 825)
+                {
+                    Q.Cast(Sender.ServerPosition);
+                }
             } 
         }
 
