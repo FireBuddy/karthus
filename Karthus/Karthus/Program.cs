1d621373e06042c8aa8aedeaadafd4faa998054d
AkumaCORE <k-sintu@hotmail.com>
2016-05-24 10:53:43 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -828,7 +828,7 @@ namespace Karthus
                         EntityManager.MinionsAndMonsters.EnemyMinions.Where(
                             x =>
                             x.Distance(Player.Instance) <= Q.Range && x.Health > 5 && !x.IsDead && x.IsValid
-                            && (Prediction.Health.GetPrediction(x, (int)(Q.CastDelay * 1000))
+                            && (Prediction.Health.GetPrediction(x, (int)(Q.CastDelay = 1000))
                                 > player.GetSpellDamage(x, SpellSlot.Q)))
                             .Select(xm => xm.ServerPosition.To2D())
                             .ToList(),
