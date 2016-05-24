08655757fdeb44a98313a74870cff78292e57518
AkumaCORE <k-sintu@hotmail.com>
2016-05-24 12:02:47 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -834,7 +834,7 @@ namespace Karthus
                         Q.Width + 100,
                         Q.Range);
 
-                if (Q.IsReady() && location.MinionsHit < 1)
+                if (Q.IsReady() && location.MinionsHit <= 1)
                 {
                     Q.Cast(location.Position.To3D());
                 }
