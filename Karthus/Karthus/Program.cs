daae858d28058c7f0126426c57b40c18cf5f9c5f
AkumaCORE <k-sintu@hotmail.com>
2016-05-29 22:19:10 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -740,7 +740,7 @@ namespace Karthus
                         }
                     }
 
-                    if (qTarget == null || (!qm || !Q.IsReady() || !qTarget.IsValidTarget(Q.Range - 40)))
+                    if (qTarget == null || (!qm || !Q.IsReady() || !qTarget.IsValidTarget(Q.Range - 25)))
                     {
                         return false;
                     }
