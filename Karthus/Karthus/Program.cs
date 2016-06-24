bcdee38e80134e92bf559d7088242958d24710f3
FireBuddy <aijun@live.ca>
2016-06-24 17:36:57 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -191,6 +191,7 @@ namespace Karthus
         
         private static void Obj_AI_Base_OnBasicAttack(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
+            var flags = Orbwalker.ActiveModesFlags;
             if (sender == null || (!flags.HasFlag(Orbwalker.ActiveModes.LastHit)))
             {
                return;
