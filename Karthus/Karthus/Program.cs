1fd3a29b4423ab71742ed7392f96396f059e3e08
FireBuddy <aijun@live.ca>
2016-06-24 13:09:20 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -201,11 +201,11 @@ namespace Karthus
                 
                 if (Minion.Distance(Player.Instance.Position) < Minion.Distance(sender.Position))
                 {
-                  Q.Cast(sender.ServerPosition + Minion.Distance(sender.ServerPosition));
+                  Q.Cast(sender.ServerPosition - Minion.Distance(sender.ServerPosition));
                 }
                 else if(Minion.Distance(Player.Instance.Position) > Minion.Distance(sender.Position))
                 {
-                  Q.Cast(sender.ServerPosition - Minion.Distance(sender.ServerPosition));
+                  Q.Cast(sender.ServerPosition + Minion.Distance(sender.ServerPosition));
                 }
 
             }
