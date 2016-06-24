ddd643f0f07564b570c9d1f70d3fa0725ca165f8
FireBuddy <aijun@live.ca>
2016-06-24 16:46:10 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -583,7 +583,7 @@ namespace Karthus
                         if (Minion != null)
                         {
                         
-                        var RPos = new Vector3(qTarget.ServerPosition.X - Minion.ServerPosition.X ,  qTarget.ServerPosition.Y, qTarget.ServerPosition.Z);  
+                        var RPos = new Vector3(qTarget.ServerPosition.X ,  qTarget.ServerPosition.Y, qTarget.ServerPosition.Z);  
                         Q.Cast(RPos);
                         } 
                     }
