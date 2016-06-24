c83ddaed9377dd1082ffdb9b93bb95b4b7ae93eb
FireBuddy <aijun@live.ca>
2016-06-24 15:07:54 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -196,14 +196,14 @@ namespace Karthus
                return;
             }
             CurrentTarget = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
-            var Minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Both,sender.ServerPosition, 70);
+            var Minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Both,sender.ServerPosition, 200);
             foreach (var Minion in Minions)
             if (sender == CurrentTarget && !sender.IsDashing() && sender.Type == GameObjectType.AIHeroClient && sender.IsValidTarget(Q.Range) && Q.IsReady() && sender.IsEnemy)
             {
                 
-
+                Q.SourcePosition = Minion.ServerPosition;
                 {
-                  Q.SourcePosition = Minion.ServerPosition;
+                  
                   Q.Cast(sender.ServerPosition - 60);
                 }
 
