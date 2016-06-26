c450c5822a694d2cdceb892b0c2bfb4f0cff96d9
FireBuddy <aijun@live.ca>
2016-06-26 17:16:12 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -826,7 +826,7 @@ namespace Karthus
                     GetBestCircularFarmLocation(
                         EntityManager.MinionsAndMonsters.GetJungleMonsters()
                             .Where(x => x.Distance(Player.Instance) <= Q.Range)
-                            .Select(Q.GetPrediction(xm); xm => xm.CastPosition.To2D())
+                            .Select(Q.GetPrediction(xm); xm => xm.ServerPosition.To2D())
                             .ToList(),
                         Q.Width,
                         Q.Range);
