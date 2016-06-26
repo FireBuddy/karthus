5da1128752c8dd664e80cc59b929bf88d141cd43
FireBuddy <aijun@live.ca>
2016-06-26 13:56:02 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -901,13 +901,13 @@ namespace Karthus
 
                 if (Q.IsReady() && locationtwo.MinionsHit > 0)
                 {
-                    var Positions = RotatedPositions(locationtwo.Position.To3D(), Game.CursorPos * -1, 360, 10, 100);
-                    foreach ( var rotatedPosition in posList)
                     {
-                        var Minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Both,rotatedPosition, 110).Count();
+                        var posList = new List<Vector3>();
+                        var locationtwozero = locationtwo - (locationtwo.X + 100);
+                        var Minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Both,locationtwo, 110).Count();
                         {
                            if (Minions > 1) return; 
-                           Q.Cast(rotatedPosition);
+                           
                         }
                         
                     }
