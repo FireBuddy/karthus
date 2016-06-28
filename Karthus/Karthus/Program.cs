01e7d980f6fcf2f7029d85c019e4a140281312b5
FireBuddy <aijun@live.ca>
2016-06-27 20:05:16 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -790,7 +790,7 @@ namespace Karthus
                         return false;
                     }
                     var predQ = Q2.GetPrediction(qTarget);
-                    var RDPos = new vector3.Normalize(PredQ.CastPosition - Player.Instance.ServerPosition);
+                    var RDPos = new vector3.Normalize(PredQ.CastPosition - Player.Instance.ServerPosition) * 50;
                     var RDPos2 = qTarget.ServerPosition.Y - Player.Instance.ServerPosition.Y;
                     var RPos = new Vector3(predQ.CastPosition.X, predQ.CastPosition.Y + RDPos2, predQ.CastPosition.Z);
                     if (!cz && predQ.HitChance >= HitChance.High)
