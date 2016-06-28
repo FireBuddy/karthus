f7b440ac6f77b374888f6882765ef766cce55f97
FireBuddy <aijun@live.ca>
2016-06-27 20:52:48 -0400

    Update Program.cs

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -799,13 +799,13 @@ namespace Karthus
 
                         if (ObjectManager.Player.Position.Distance(qTarget.ServerPosition) <= 900)   
                             {
-                                    Q.Cast(160 + predQ.CastPosition);
+                                    Q.Cast(140 + predQ.CastPosition);
                             }
 
                     }
                     else
                     {
-                        Q.Cast(150 + predQ.CastPosition);
+                        Q.Cast(140 + predQ.CastPosition);
                     }
                 }
             }
